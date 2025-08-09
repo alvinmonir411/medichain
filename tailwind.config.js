b047f60b3a9158cf03dd277dda0424edf8071730
alvinmonir411 <moniruzzaman710@outlook.com>
2025-08-10 00:31:53 +0600

    add taildin utility class

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        btn: "#EF4444 ", // Deep blue for buttons
+        bg: "#EF4444 ", // Soft background
+        heading: "#1F2937", // Dark gray for headings
+        text: "#4B5563", // Medium gray for body text
+      },
+    },
+  },
+  plugins: [],
+};
